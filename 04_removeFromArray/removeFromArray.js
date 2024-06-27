399f7db3f90f9647587207e550a64e79659ddf49
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-06-27 17:02:34 -0500

    Edit 04_removeFromArray/removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    if(array.length === 0) {
+        return array;
+    }
+
+    const removedArray = array;
+    for(let i = 0; i < removedArray.length; i++) {
+        if(removedArray[i] === remove) {
+            removedArray.splice(i, 1);
+        }
+    }
+    return removedArray;
 };
 
 // Do not edit below this line
