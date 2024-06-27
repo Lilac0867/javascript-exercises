d12947a1da1e9954e3b7ee85527ecb8bad73aff3
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-06-27 17:13:45 -0500

    Edit 04_removeFromArray/removeFromArray.js
    Nested loop to check through potential items needed to be removed,
    as to match the test requirements, allowing for multiple items that
    need to be removed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
-const removeFromArray = function(array, remove) {
+const removeFromArray = function(array, ...items) {
     if(array.length === 0) {
         return array;
     }
 
     const removedArray = array;
     for(let i = 0; i < removedArray.length; i++) {
-        if(removedArray[i] === remove) {
-            removedArray.splice(i, 1);
+        for(const item of items) {
+            if(removedArray[i] === item) {
+                removedArray.splice(i, 1);
+                i--; // Keeps program from skipping the next index
+            }
         }
     }
     return removedArray;
