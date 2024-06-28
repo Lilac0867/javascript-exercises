1cb93f7af843b06eda93d9a9b177bdc6ea832419
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-06-28 06:43:56 -0500

    Edit 06_leapYears/leapYears.js to match test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    if(leapYear % 4 === 0 
+    && (leapYear % 100 !== 0
+    || leapYear % 400 === 0)) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
