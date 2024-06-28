1158b72854d0dcea6916a9dac55a4e005cfb0e79
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-06-28 06:57:13 -0500

    Edit 07_tempConversion/tempConversion.js added code to convertToCelsius to match test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // C = (F - 32) * 5/9
+  const celsius = (fahrenheit - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10;
 };
 
 const convertToFahrenheit = function() {
+  // F = (C * 9/5) + 32
 };
 
 // Do not edit below this line
