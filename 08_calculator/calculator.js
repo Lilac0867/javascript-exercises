c61e03bbfde7b21ad6eaaeaed8b4be2699e863b3
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-09-23 13:01:43 -0500

    Edit calculator.js to match test
    Original solution was for multiplying numbers together, this solution
    actually adds the contents of an array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -9,11 +9,11 @@ const subtract = function(a, b) {
   return a - b;
 };
 
-const sum = function(a, b) {
-	if(a === undefined || b === undefined) {
-    a, b = 0;
+const sum = function(arr) {
+	if(arr === undefined) {
+    arr = [0];
   }
-  return a * b;
+  return arr.reduce(((total, nextNum) => total + nextNum), 0);
 };
 
 const multiply = function() {
