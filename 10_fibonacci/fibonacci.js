9f4a6162018b4e3dbc61580cd17bd58afa56096c
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-09-23 18:56:59 -0500

    Edit fibonacci.js to pass test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const fib = [1, 1];
+    for(let i = 2; i < num; i++) {
+        fib.push(fib[i-1] + fib[i-2]);
+    }
+    return fib[num-1];
 };
 
 // Do not edit below this line
