5a254f99e04cb96b9258d8a7c9aa34def1e07d82
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-09-23 18:03:30 -0500

    Edit palindromes.js to pass test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let inc = 0;
+    if(str.length % 2 !== 0) {
+      inc = 1;
+    }
 
+    let startStr = str.toLowerCase().slice(0, (str.length/2));
+    let endStr = str.toLowerCase().slice((str.length/2)+inc)
+                                  .split('')
+                                  .reverse()
+                                  .toString()
+                                  .replaceAll(',', '');
+
+    if(startStr === endStr) {
+      return true;
+    }
 };
 
 // Do not edit below this line
