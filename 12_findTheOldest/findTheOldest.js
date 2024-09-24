f2f87c8a4ac9ef6f7842f8f559ef81b66cb0f4c5
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-09-24 10:27:18 -0500

    Edit findTheOldest.js to pass test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const peopleAge = people.map((person) => {
+        const name = person.name;
+        const age = person.yearOfDeath - person.yearOfBirth;
+        return {name, age};
+    });
 
+    const sortedAge = peopleAge.sort((a, b) => {
+        // a is previous, b is current
+        // - is ab, + is ba
+        if(a.age > b.age) {
+            return -1;
+        } 
+        return 1;
+    });
+    
+    return sortedAge[0];
 };
 
 // Do not edit below this line
